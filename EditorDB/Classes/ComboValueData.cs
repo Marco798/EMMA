e867a67cd70c96ccdc17e3bda104a1fa42deab88
Marco <marcoallegri117@gmail.com>
2024-09-07 22:02:51 +0200

    EditorDB - aggiunta form per l'inserimento di nuove combo

---
+++ EditorDB/Classes/ComboValueData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EditorDB.Classes {
+	public class ComboValueData {
+		public int Index { get; set; }
+		public string Name { get; set; }
+		public string Value { get; set; }
+
+		public ComboValueData() { }
+
+		public ComboValueData(int index) {
+			Index = index;
+			Name = string.Empty;
+			Value = string.Empty;
+		}
+
+		public ComboValueData Clone() {
+			return new ComboValueData() {
+				Index = Index,
+				Name = Name,
+				Value = Value
+			};
+		}
+
+		public override bool Equals(object? obj) {
+			return obj is ComboValueData data &&
+				   Index == data.Index &&
+				   Name == data.Name &&
+				   Value == data.Value;
+		}
+
+		public override int GetHashCode() {
+			HashCode hash = new();
+			hash.Add(Index);
+			hash.Add(Name);
+			hash.Add(Value);
+			return hash.ToHashCode();
+		}
+	}
+}
