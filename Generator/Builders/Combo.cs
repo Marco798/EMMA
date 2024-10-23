e804d72fcf361722c1956f17e551a0a8eed4828c
Marco <marcoallegri117@gmail.com>
2024-10-23 23:30:25 +0200

    Generator - Aggiunti metodi GetNames e GetValues nel generatore Combo

--- Generator/Builders/Combo.cs
+++ Generator/Builders/Combo.cs
@@ -5,10 +5,12 @@
 
 		private static string pattern_Main = string.Empty;
 		private static string pattern_ValueField = string.Empty;
+		private static string pattern_GetValueField = string.Empty;
 
-		private static string valueField_List = string.Empty;
+        private static string valueField_List = string.Empty;
+        private static string getValueField_List = string.Empty;
 
-		public static void Generate() {
+        public static void Generate() {
 			const string folder = @"Combo\";
 			directory = Consts.generatedDirectory + folder;
 			string pattern = Consts.patternDirectory + folder;
@@ -19,9 +21,10 @@
 			pattern_Main = File.ReadAllText(pattern + "Main.txt");
 
 			pattern_ValueField = File.ReadAllText(pattern + "ValueField.txt");
-			#endregion
+            pattern_GetValueField = File.ReadAllText(pattern + "GetValueField.txt");
+            #endregion
 
-			foreach (Combo_Record combo_record in combo_RecordList) {
+            foreach (Combo_Record combo_record in combo_RecordList) {
 				ComboElaboration(combo_record);
 			}
 
@@ -34,15 +37,17 @@
 
 			#region Sections declaration
 			valueField_List = string.Empty;
-			#endregion
+            getValueField_List = string.Empty;
+            #endregion
 
-			foreach (ComboValues_Record comboValues_record in comboValues_RecordList.Where(x => x.COMBO == combo_record.NAME)) {
+            foreach (ComboValues_Record comboValues_record in comboValues_RecordList.Where(x => x.COMBO == combo_record.NAME)) {
 				ComboValueElaboration(comboValues_record);
 			}
 
 			_Main = _Main.Replace("%%VALUE_FIELD%%", valueField_List[..^4]);
+            _Main = _Main.Replace("%%GET_VALUE_FIELD%%", getValueField_List[..^3]);
 
-			_Main = _Main.Replace("%%NAME_SPACE%%", "EMMA_BE.Generated");
+            _Main = _Main.Replace("%%NAME_SPACE%%", "EMMA_BE.Generated");
 			_Main = _Main.Replace("%%COMBO_NAME%%", combo_record.NAME);
 
 			File.WriteAllText(directory + $"{combo_record.NAME}_Combo.cs", _Main);
@@ -53,7 +58,12 @@
 			string valueField = pattern_ValueField;
 			valueField = valueField.Replace("%%COMBO_VALUE_VALUE%%", comboValue_record.VALUE);
 			valueField_List += valueField.Replace("%%COMBO_VALUE_NAME%%", comboValue_record.NAME) + $"\r\n";
-			#endregion
-		}
-	}
+            #endregion
+
+            #region GetValueField
+            string getValueField = pattern_GetValueField;
+            getValueField_List += getValueField.Replace("%%COMBO_VALUE_NAME%%", comboValue_record.NAME) + $"\r\n";
+            #endregion
+        }
+    }
 }
